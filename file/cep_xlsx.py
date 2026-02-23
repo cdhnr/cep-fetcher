47675535ac7cc5b86786638f1b9f523de39a10ba
cdhnr <cdhnr@icloud.com>
2026-02-23 13:39:29 -0300

    corrections to main.py and cep_xlsx.py

--- file/cep_xlsx.py
+++ file/cep_xlsx.py
@@ -2,7 +2,7 @@ import pandas as pd
 from service.cep_service import buscar_uf
 from concurrent.futures import ThreadPoolExecutor
 
-def p_xlsx(filepath="cep.xlsx"):
+def p_xlsx(filepath="file/cep.xlsx"):
     df = pd.read_excel(filepath)
     ceps = df["CEP"].astype(str).str.zfill(8).unique()
 
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from file.cep_xlsx import p_xlsx
 
 if __name__ == "__main__":
-    p_xlsx("cep.xlsx")
+    p_xlsx("file/cep.xlsx")
